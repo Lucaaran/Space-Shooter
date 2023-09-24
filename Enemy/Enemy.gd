b8345450999ff20e62599abcd9d0b7c487cde3a9
Lucaaran <aranaemma47@gmail.com>
2023-09-23 20:54:43 -0400

    Part 4

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -13,10 +13,10 @@ func _ready():
 	initial_position.y =y_positions[randi() % y_positions.size()]
 	position = initial_position
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	position += direction
 	position.y = initial_position.y + sin(position.x/20)*wobble
-	if position.x > 1200:
+	if position.x > Global.VP.x + 100:
 		queue_free()
 
 func _on_timer_timeout():
@@ -27,10 +27,12 @@ func _on_timer_timeout():
 		var d = global_position.angle_to_point(Player.global_position) + PI/2
 		bullet.rotation = d
 		bullet.global_position = global_position + Vector2(0, -40).rotated(d)
+		Effects.add_child(bullet)
 
 func damage(d):
 	health -= d
 	if health <= 0:
+		Global.update_score(500)
 		queue_free()
 
 func _on_area_2d_body_entered(body):
---
+++ UI/Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_restart_pressed():
+	Global.reset()
+	get_tree().change_scene_to_file("res://Player/game.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
